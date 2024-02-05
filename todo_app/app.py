34d8ca5c87946cc1c37d75e72a8b0c0ac8bc58e7
Kemi Osborne <kemiosborne@gmail.com>
2024-02-05 12:10:45 +0000

    Refactoring

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,5 @@
 from flask import Flask, redirect, render_template, request
-from todo_app.data.session_items import add_item, get_items
-import requests
-import dotenv
-import os
-
+from todo_app.data.trello_items import add_item, complete_item, get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -12,63 +8,21 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
-
-    board_id = os.getenv("TRELLO_BOARD_ID")
-
-    reqUrl = f"https://api.trello.com/1/boards/{board_id}/lists"
-
-
-    query_parameters = {
-        "key": os.getenv("TRELLO_API_KEY"),
-        "token": os.getenv("TRELLO_API_TOKEN"),
-        "cards": "open",
-        "card_fields": "id,name",
-    }
-
-    response = requests.get(reqUrl, params=query_parameters)
-    response_json = response.json()
-
-    cards = []
-    for trello_list in response_json:
-        for card in trello_list['cards']:
-            card['status'] = trello_list['name']
-            cards.append(card)
+    cards = get_items()
 
     return render_template("index.html", items = cards)
-
 
 
 @app.route('/add-item', methods = ["POST"])
 def create():
     todo_title = request.form.get('todo-name')
 
-    reqUrl = f"https://api.trello.com/1/cards"
-
-    query_parameters = {
-        "key": os.getenv("TRELLO_API_KEY"),
-        "token": os.getenv("TRELLO_API_TOKEN"),
-        "idList": "6579d6eb8b2f72513c4a97cc",
-        "name": todo_title,
-    }  
-
-    response = requests.post(reqUrl, params = query_parameters)
-
     add_item(todo_title)
     return redirect('/')
-
 
   
 @app.route('/complete-item/<item_id>', methods = ["POST"])
 def complete(item_id):
-    reqUrl = f"https://api.trello.com/1/cards/{item_id}"
-
-    query_parameters = {
-        "key": os.getenv("TRELLO_API_KEY"),
-        "token": os.getenv("TRELLO_API_TOKEN"),
-        "idList": "6579d6eb8b2f72513c4a97ce",
-    }  
-
-    response = requests.put(reqUrl, params = query_parameters)
+    complete_item(item_id)
 
     return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,6 +2,28 @@ import requests
 
 import os
 
+def get_items():
+    board_id = os.getenv("TRELLO_BOARD_ID")
+
+    reqUrl = f"https://api.trello.com/1/boards/{board_id}/lists"
+
+    query_parameters = {
+        "key": os.getenv("TRELLO_API_KEY"),
+        "token": os.getenv("TRELLO_API_TOKEN"),
+        "cards": "open",
+        "card_fields": "id,name",
+    }
+
+    response = requests.get(reqUrl, params=query_parameters)
+    response_json = response.json()
+
+    cards = []
+    for trello_list in response_json:
+        for card in trello_list['cards']:
+            card['status'] = trello_list['name']
+            cards.append(card)
+
+    return cards
 
 
 def add_item(title):
@@ -11,10 +33,25 @@ def add_item(title):
         "key": os.getenv("TRELLO_API_KEY"),
         "token": os.getenv("TRELLO_API_TOKEN"),
         "idList": os.getenv("TRELLO_TODO_LIST_ID"),
-        "name": title
-        }  
+        "name": title,
+    }  
 
     response = requests.post(reqUrl, params = query_parameters)
+
+    print(response.status_code)
+    print(response.text)
+
+
+def complete_item(item_id):
+    reqUrl = f"https://api.trello.com/1/cards/{item_id}"
+
+    query_parameters = {
+        "key": os.getenv("TRELLO_API_KEY"),
+        "token": os.getenv("TRELLO_API_TOKEN"),
+        "idList": os.getenv("TRELLO_DONE_LIST_ID"),
+    }  
+
+    response = requests.put(reqUrl, params = query_parameters)
 
     print(response.status_code)
     print(response.text)
