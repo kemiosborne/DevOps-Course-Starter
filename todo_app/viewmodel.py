41f80ea4695ff554705dffe3d681fff1cf473e60
Kemi Osborne <kemiosborne@gmail.com>
2024-02-12 12:29:41 +0000

    Implement done_items method

--- todo_app/viewmodel.py
+++ todo_app/viewmodel.py
@@ -1,10 +1,26 @@
+from todo_app.data.todo_item import Item
+
+
 class ViewModel:
-    def __init__(self, items):
+    def __init__(self, items: list[Item]):
         self._items = items
  
     @property
     def items(self):
         return self._items
+    
+    @property
+    def todo_items(self):
+        return []
+    
     @property
     def done_items(self):
-        return []
+        all_items = self._items
+
+        output = []
+
+        for item in all_items:
+            if item.status == "Done":
+                output.append(item)
+
+        return output
