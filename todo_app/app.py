7a49cff0e4a0dce5997ec04a425d7b8d56da0726
Kemi Osborne <kemiosborne@gmail.com>
2024-02-19 11:43:13 +0000

    Integration testing

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,28 +3,31 @@ from todo_app.data.trello_items import add_item, complete_item, get_items
 from todo_app.flask_config import Config
 from todo_app.viewmodel import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
 
-@app.route('/')
-def index():
-    cards = get_items()
+    @app.route('/')
+    def index():
+        cards = get_items()
 
-    item_view_model = ViewModel(cards)
-    return render_template('index.html', view_model=item_view_model)
+        item_view_model = ViewModel(cards)
+        return render_template('index.html', view_model=item_view_model)
 
 
-@app.route('/add-item', methods = ["POST"])
-def create():
-    todo_title = request.form.get('todo-name')
+    @app.route('/add-item', methods = ["POST"])
+    def create():
+        todo_title = request.form.get('todo-name')
 
-    add_item(todo_title)
-    return redirect('/')
+        add_item(todo_title)
+        return redirect('/')
 
-  
-@app.route('/complete-item/<item_id>', methods = ["POST"])
-def complete(item_id):
-    complete_item(item_id)
+    
+    @app.route('/complete-item/<item_id>', methods = ["POST"])
+    def complete(item_id):
+        complete_item(item_id)
 
-    return redirect('/')
+        return redirect('/')
+
+    return app
